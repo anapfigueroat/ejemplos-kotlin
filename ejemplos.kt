d997db8570bcd1dcfddd6bc4c824fef926f80ecd
anapfigueroat <117108934+anapfigueroat@users.noreply.github.com>
2024-06-13 19:42:20 -0600

    Update ejemplos.kt

--- ejemplos.kt
+++ ejemplos.kt
@@ -2,41 +2,45 @@
 // Ejemplos extraidos de ChatGPT
 
 // Paradigma de programacion orientada a objetos
-open class Animal {
-    open fun sound() {
+open class Animal { // Definicion de clase llamada "Animal"
+    open fun sound() { // Definicion de una funcion de la clase
         println("Animal sound")
     }
 }
 
-class Dog : Animal() {
-    override fun sound() {
+class Dog : Animal() { // Definicion de clase "Dog" que hereda de clase "Animal"
+    override fun sound() { // Sobrescribe la función sound para proporcionar una implementación específica para el perro.
         println("Woof")
     }
 }
 
 fun main() {
-    val myDog = Dog()
-    myDog.sound()  // Salida: Woof
+    val myDog = Dog() // Crea una instancia de la clase Dog.
+    myDog.sound()   // Llama a la función sound de la instancia de Dog, que ejecutará la versión sobrescrita de la función.
 }
 
 // Paradigma de programacion funcional
-fun operation(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+
+fun operation(x: Int, y: Int, operation: (Int, Int) -> Int): Int { // Define una función de orden superior llamada 'operation'.
+                                                                   // Esta función recibe tres parámetros: dos enteros 'x' e 'y', y una función 'operation' que toma dos enteros y devuelve un entero.
     return operation(x, y)
 }
 
-val sum = operation(10, 5) { a, b -> a + b }
+val sum = operation(10, 5) { a, b -> a + b } // Pasa los valores 10 y 5 como los dos primeros parámetros y una lambda como el tercer parámetro.
+                                             // La lambda { a, b -> a + b } toma dos parámetros 'a' y 'b' y devuelve su suma.
 println(sum)  // Salida: 15
 
 
-val numbers = listOf(1, 2, 3, 4, 5)
-val doubled = numbers.map { it * 2 }
+val numbers = listOf(1, 2, 3, 4, 5) // Crea una lista inmutable de números enteros del 1 al 5.
+val doubled = numbers.map { it * 2 } // Aplica la función de orden superior 'map' para duplicar cada número en la lista.
+                                     // 'it' es la referencia implícita a cada elemento de la lista.
 println(doubled)  // Salida: [2, 4, 6, 8, 10]
 
 
 // Paradigma de programacion imperativa
-var sum = 0
-for (i in 1..10) {
-    sum += i
+var sum = 0 // Declara variable "sum" con valor de 0
+for (i in 1..10) { // Crea un loop en el cual "i" itera de 1 a 10
+    sum += i // Suma "i" a la variable "sum" en cada iteracion
 }
 println(sum)  // Salida: 55
 
